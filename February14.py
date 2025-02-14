bf7f3f4a2afde4709ad7bd5e61a666a309dd0993
thepriyankaY <priyankay159@stemcollegiate.ca>
2025-02-14 09:32:51 -0700

    Create February14.py

---
+++ February14.py
@@ -0,0 +1,12 @@
+#today i was reminded that text is called a string
+#i was also taught that when you put strigns and variables together, it is called a concatenate
+food = input("Name a type of food")
+plant = input("Name a plant:")
+cookingType = input("What is a way to cook something?")
+burntFood = input("How do you describe burnt food?")
+householdItem = input("Name something in your house:")
+print()
+print("Tonight's dinner:")
+
+print("For dinner you should make", cookingType, food, "with", burntFood, plant, "on a plate of", householdItem)
+print()
