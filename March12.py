90826bb0a643f986e875378126889f231b8d9725
thepriyankaY <priyankay159@stemcollegiate.ca>
2025-03-12 10:21:07 -0600

    Create March12.py

---
+++ March12.py
@@ -0,0 +1,35 @@
+#Day 8
+#i learned that through python i can help boost peoples day through affirmations (i don't know what to write and im making it look like im writing what tomi wants me to write)
+
+print("Hello. Welcome to your favoite affirmation generator.")
+name = input("What is your name? ")
+
+
+
+if name =="terkper" or name == "terkper":
+ DOW = input("What is the day of the week? ")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a great week, with a great start", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("i hope you have a good day", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("you're halfway there", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"your week is almost over!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "It's FRIDAY! FINALLY! THE WEEKEND!")
+
+elif name == "Hannah" or name== "hannah":
+ DOW = input("What is the day of the week? ")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a great Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("You look great in that color", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("You look chipper today", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"you are doing a great job!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "it's FRIDAY!")
+else:
+ print("I do not remember your name, but I hope you are having a great day!")
